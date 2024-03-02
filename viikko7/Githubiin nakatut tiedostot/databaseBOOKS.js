fa29ae8c71a9a1856508e16e9700837161756b8a
Braltzu <156204503+Braltzu@users.noreply.github.com>
2024-03-02 21:26:07 +0200

    Delete viikko7/Githubiin nakatut tiedostot/databaseBOOKS.js

--- viikko7/Githubiin nakatut tiedostot/databaseBOOKS.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql2');
-const connection = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'ylijumal',
-  password: 'saku',
-  database: 'books'
-});
-module.exports = connection;
